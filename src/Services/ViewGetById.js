f456c5eaf5ac655520c3465221f9c93ccb41c347
Huzaifa246 <huzaifa24680@gmail.com>
2024-05-17 00:13:04 +0500

    Updated Link

--- src/Services/ViewGetById.js
+++ src/Services/ViewGetById.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 async function ViewGetByIdApi(id) {
   try {
-    const response = await axios.get(`http://localhost:3000/laptops/${id}`);
+    const response = await axios.get(`https://laptop-inventory-backend.vercel.app/laptops/${id}`);
     return response;
   } catch (error) {
     console.error('Error viewing laptop:', error);
