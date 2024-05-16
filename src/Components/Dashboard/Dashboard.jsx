d717c1a246c13dce62dce900beaf69a270de4273
Huzaifa246 <huzaifa24680@gmail.com>
2024-05-16 23:11:10 +0500

    Updated a tag to LINK

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,19 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import banner from '../../assets/images/banner.jpg';
-// import banner2 from '../../assets/images/Banner2.jpg';
 import banner3 from '../../assets/images/banner3.jpg';
-// import Card1 from "../../assets/images/card1.jpg";
-// import Card2 from "../../assets/images/card2.jpg";
-// import Card3 from "../../assets/images/card3.jpg";
-// import Card4 from "../../assets/images/card4.jpg";
-// import FeaturedCards from './../FeaturedProduct/FeaturedCards';
 import Banner from '../ReuseableComponents/banner';
 import FeaturedProducts from '../FeaturedProduct/FeaturedProducts';
 import FaqsPage from './../Faqs/FaqsPage';
 import ContactForm from './../ContactUs/contactForm';
 import Footer from './../Footer/footer';
 // import fetchAllLaptops from './../../Services/getAllLaptops';
-
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
 
@@ -39,11 +33,11 @@ const Dashboard = () => {
       <Banner imageUrl={banner} buttonText="Shop All" />
       <FeaturedProducts />
       <div className='d-flex justify-content-center my-4'>
-        <a href="/featured-products">
+        <Link to="/featured-products">
           <button className='view-all-btn'>
             View All
           </button>
-        </a>
+        </Link>
       </div>
       <Banner imageUrl={banner3} buttonText={false} />
       <FaqsPage />
