2c772a95d6735a8c5724f7d4bdaf2e380410df2a
Huzaifa246 <huzaifa24680@gmail.com>
2024-06-07 00:29:41 +0500

    updated viteconfig file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build', // This sets the output directory for the build files
+  },
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 5000,
+  },
+});
