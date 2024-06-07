e7dd65a82c635ae184914dfd2bf5dc04b2b13197
Huzaifa246 <huzaifa24680@gmail.com>
2024-06-07 23:18:34 +0500

    testing

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build', // This sets the output directory for the build files
-  },
-  server: {
-    port: 3000,
-  },
-  preview: {
-    port: 5000,
-  },
-});
+})
